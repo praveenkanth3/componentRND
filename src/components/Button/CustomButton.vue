91204d35e08f6e38518284943852f3b9b26655bf
Praveenkanth S <praveenkanth.s@zohocorp.com>
2024-05-23 16:43:42 +0530

    changes

---
+++ src/components/Button/CustomButton.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <div class='btnContainer'>
+        <button @click="onClickBtn" :class="customClass">{{ label }}</button>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'CustomButton',
+
+    props: {
+        label: {
+            type: String,
+            required: true,
+        },
+
+        onClickBtn: {
+            type: Function,
+            required: true,
+        },
+
+        customClass: {
+            type: String,
+            required: false
+        }
+    }
+}
+
+</script>
+
+<style>
+
+button {
+    width:fit-content;
+}
+
+.btnContainer {
+    display: flex;
+    justify-content: center;
+}
+
+</style>
---
+++ src/components/InputBox/InputBox.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="inputContainer">
+        <div class="label">{{ label  }}</div>
+        <input class="input" :type="inputType" :placeholder="placeholder" :value="value" @input="onChange($event.target.value)" :maxlength="maxlength"/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'InputBox',
+
+    props:{
+
+        label:{
+            type: String,
+            required: false
+        },
+
+        placeholder: {
+            type: String,
+            required: false
+        },
+
+        value: {
+            required:true
+        },
+        
+        inputType: {
+            type: String,
+            required: false,
+            default: 'text'
+        },
+
+        onChange: {
+            type: Function,
+            required: true
+        },
+
+        maxlength: {
+            type: String,
+            required: false,
+            default: '9999'
+        }
+    }
+}
+
+</script>
+
+<style>
+
+.inputContainer {
+    display: flex;
+    column-gap: 10px;
+}
+
+.input{
+    width: 150px;
+    border-radius: 10px;
+    height: 30px;
+    border: 1px solid gray;
+}
+
+.input:focus-visible {
+    outline: none;
+}
+
+.label {
+    font-weight: bold;
+    text-align: center;
+    padding: 5px;
+}
+
+</style>
