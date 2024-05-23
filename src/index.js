3f3311120d5ad15d38c8e6856d7233aed03a6633
Praveenkanth S <praveenkanth.s@zohocorp.com>
2024-05-23 16:45:41 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import HelloWorld from "./components/HelloWorld/HelloWorld.vue";
 import CustomButton from "./components/Button/CustomButton.vue";
 import InputBox from "./components/InputBox/InputBox.vue";
-import PageNotFound from "./components/PageNotFound/PageNotFound.vue";
 import SelectBox from "./components/Select/SelectBox.vue";
 import CardComponent from "./components/CardComponent/CardComponent.vue";
 
-export {HelloWorld,CustomButton,InputBox,PageNotFound,SelectBox,CardComponent }
+export {HelloWorld,CustomButton,InputBox,SelectBox,CardComponent }
